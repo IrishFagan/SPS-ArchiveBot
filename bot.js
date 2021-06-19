308fe100256e568adb2609e0f396a0ee53a2d19d
Irish Fagan <IrishFagan@live.com>
2021-06-19 14:00:48 -0700

    Add error checking for archive posts without an image

--- bot.js
+++ bot.js
@@ -9,18 +9,26 @@ function archiveCommand(message) {
 }
 
 function imageAttached(message) {
-	return message.attachments.array()[0].url
+	return message.attachments.array()[0] !== undefined
 }
 
 function properArchivePost(message) {
 	return (archiveCommand(message) && imageAttached(message))
 }
 
+function notArchiveBot(message) {
+	return message.author !== 'ArchiveBot'
+}
+
 client.on("message", (message) => {
-	if(properArchivePost(message)) {
+	if(properArchivePost(message) && notArchiveBot(message)) {
 		message.reply("Archived!")
 		const archiveChannel = client.channels.cache.find(channel => channel.name === 'bike-archive')
-		console.log(message.attachments)
 		archiveChannel.send(message.attachments.array()[0].url)
+		archiveChannel.send(message.author.username)
+	}
+
+	if(archiveCommand(message) && !imageAttached(message) && notArchiveBot(message)) {
+		message.reply("Not a proper archive post.\nPlease attach an image for it to moved to 'bike-archive'!")
 	}
 })

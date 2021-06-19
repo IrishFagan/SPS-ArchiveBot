754811c96b8330e51ea018e59379f2b317078bc6
Irish Fagan <IrishFagan@live.com>
2021-06-19 13:35:08 -0700

    Add functionality to send attachment url to archive channel

--- bot.js
+++ bot.js
@@ -8,10 +8,15 @@ function archiveCommand(message) {
 	return message.content === "!archive"
 }
 
+function imageAttached(message) {
+	return message.attachments.array()[0].url
+}
+
 client.on("message", (message) => {
-	if(archiveCommand(message)) {
+	if(archiveCommand(message) && imageAttached(message)) {
 		message.reply("Archived!")
 		const archiveChannel = client.channels.cache.find(channel => channel.name === 'bike-archive')
-		archiveChannel.send('Cool!')
+		console.log(message.attachments)
+		archiveChannel.send(message.attachments.array()[0].url)
 	}
 })

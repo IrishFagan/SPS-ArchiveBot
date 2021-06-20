88712b2b5899d63c33ead9015bdc7db452235f05
Irish Fagan <IrishFagan@live.com>
2021-06-20 16:53:20 -0700

    Update bot to utilize environment variables. Fix regex matching

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const linkRegex = new RegExp("http[A-Za-z0-9_\/\:.]*(.jpg|.png)")
 client.login(process.env.TOKEN)
 
 function archiveCommand(message) {
-	return message.content.includes("!archive")
+	return message.content.includes(process.env.COMMAND)
 }
 
 function imageAttached(message) {
@@ -26,7 +26,7 @@ function attachedLinkArchivePost(message) {
 	return (archiveCommand(message) && linkAttached(message))
 }
 
-function archiveCommandWithoutImage(message) {
+function archiveCommandWithoutImageOrLink(message) {
 	return (archiveCommand(message) && !imageAttached(message) && !linkAttached(message) && notArchiveBot(message))
 }
 
@@ -35,10 +35,9 @@ function notArchiveBot(message) {
 }
 
 client.on("message", (message) => {
-	const archiveChannel = client.channels.cache.find(channel => channel.name === 'bike-archive')
+	const archiveChannel = client.channels.cache.find(channel => channel.name === process.env.CHANNEL)
 
 	if(attachedImageArchivePost(message)) {
-
 		message.reply("Archived!")
 		
 		archiveChannel.send(message.attachments.array()[0].url)
@@ -48,11 +47,11 @@ client.on("message", (message) => {
 	if(attachedLinkArchivePost(message)) {
 		message.reply("Archived!")
 
-		archiveChannel.send(message.content.match(linkRegex))
-		archiveChannel.send(message.author.username)
+		archiveChannel.send(message.content.match(linkRegex)[0])
+		archiveChannel.send(`Shared by ${message.author.username}`)
 	}
 
-	if(archiveCommandWithoutImage(message)) {
+	if(archiveCommandWithoutImageOrLink(message)) {
 		message.reply("Not a proper archive post.\nPlease attach an image for it to be moved to 'bike-archive'!")
 	}
 })

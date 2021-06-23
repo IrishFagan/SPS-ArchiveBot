72d94e3e271c1ababb48e901a1bd7d044b5a0d73
Irish Fagan <IrishFagan@live.com>
2021-06-23 15:26:33 -0700

    Refactor slightly to watch for reactions on messages

--- bot.js
+++ bot.js
@@ -30,37 +30,35 @@ function archiveCommandWithoutImageOrLink(message) {
 	return (archiveCommand(message) && !imageAttached(message) && !linkAttached(message))
 }
 
-function notArchiveBot(message) {
-	return message.author !== 'ArchiveBot'
+function sendArchiveMessage(message, attachment, author) {
+	const archiveChannel = client.channels.cache.find(channel => channel.name === process.env.CHANNEL)
+
+	message.reply("Archived!")
+
+	archiveChannel.send(attachment)
+	archiveChannel.send(author)
 }
 
 function checkArchiveMessage(message) {
-	const archiveChannel = client.channels.cache.find(channel => channel.name === process.env.CHANNEL)
+	console.log('checking')
 
 	if(attachedImageArchivePost(message)) {
-		message.reply("Archived!")
-		
-		archiveChannel.send(message.attachments.array()[0].url)
-		archiveChannel.send(message.author.username)
+		console.log('attached image')
+		sendArchiveMessage(
+			message,
+			message.attachments.array()[0].url,
+			message.author.username
+		)
 	}
 
 	if(attachedLinkArchivePost(message)) {
-		message.reply("Archived!")
-
-		archiveChannel.send(message.content.match(linkRegex)[0])
-		archiveChannel.send(`Shared by ${message.author.username}`)
+		console.log('attached link')
+		sendArchiveMessage(
+			message,
+			message.content.match(linkRegex)[0],
+			`Shared by ${message.author.username}`
+		)
 	}
-
-	message.awaitReactions(() => ['🅰️'], { max: 1})
-		.then(collected => {
-			const reaction = collected.first();
-
-			if(reaction.emoji.name === '🅰️') {
-				message.reply("Archived!")
-				archiveChannel.send(message.attachments.array()[0].url)
-				archiveChannel.send(message.author.username)
-			}
-		})
 
 	if(archiveCommandWithoutImageOrLink(message)) {
 		message.reply("Not a proper archive post.\nPlease attach an image for it to be moved to 'bike-archive'!")
@@ -69,6 +67,19 @@ function checkArchiveMessage(message) {
 
 client.on("message", (message) => {
 	if (message.author.bot) return
+
+	message.awaitReactions(() => ['🅰️'], { max: 1})
+		.then(collected => {
+			const reaction = collected.first();
+
+			if(reaction.emoji.name === '🅰️') {
+				sendArchiveMessage(
+					message,
+					message.attachments.array()[0].url,
+					message.author.username
+				)
+			}
+		})
 
 	checkArchiveMessage(message)
 })

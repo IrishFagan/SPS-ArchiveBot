9a3b1da2876adb93d86307c1999501b74f34c335
Irish Fagan <IrishFagan@live.com>
2021-06-23 14:15:43 -0700

    Refactor archive message checking and handling

--- bot.js
+++ bot.js
@@ -30,20 +30,12 @@ function archiveCommandWithoutImageOrLink(message) {
 	return (archiveCommand(message) && !imageAttached(message) && !linkAttached(message))
 }
 
-client.on("message", (message) => {
-	if (message.author.bot) return
+function notArchiveBot(message) {
+	return message.author !== 'ArchiveBot'
+}
 
-	message.awaitReactions(() => ['🅰️'], { max: 1})
-		.then(collected => {
-			const reaction = collected.first();
-
-			if(reaction.emoji.name === '🅰️') {
-				message.reply("Archived!")
-
-				archiveChannel.send(message.attachments.array()[0].url)
-				archiveChannel.send(message.author.username)
-			}
-		})
+function checkArchiveMessage(message) {
+	const archiveChannel = client.channels.cache.find(channel => channel.name === process.env.CHANNEL)
 
 	if(attachedImageArchivePost(message)) {
 		message.reply("Archived!")
@@ -59,7 +51,24 @@ client.on("message", (message) => {
 		archiveChannel.send(`Shared by ${message.author.username}`)
 	}
 
+	message.awaitReactions(() => ['🅰️'], { max: 1})
+		.then(collected => {
+			const reaction = collected.first();
+
+			if(reaction.emoji.name === '🅰️') {
+				message.reply("Archived!")
+				archiveChannel.send(message.attachments.array()[0].url)
+				archiveChannel.send(message.author.username)
+			}
+		})
+
 	if(archiveCommandWithoutImageOrLink(message)) {
 		message.reply("Not a proper archive post.\nPlease attach an image for it to be moved to 'bike-archive'!")
 	}
+}
+
+client.on("message", (message) => {
+	if (message.author.bot) return
+
+	checkArchiveMessage(message)
 })

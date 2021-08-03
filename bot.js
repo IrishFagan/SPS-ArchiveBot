0053e61b10a0c57a49e3bb9ef401413d5d424840
Irish Fagan <IrishFagan@live.com>
2021-08-03 12:40:42 -0700

    Fix check for archive reaction count

--- bot.js
+++ bot.js
@@ -66,14 +66,13 @@ function checkArchiveMessage(message) {
 client.on("message", (message) => {
 	if (message.author.bot) return
 
-	collector = message.createReactionCollector(() => ['🅰️'], { max: 100})
+	collector = message.createReactionCollector(() => ['🅱️'], { max: 100})
 		
 	collector.on('collect', (reaction, reactionCollector) => {
-			archiveReaction = collector.collected.array().some(reacton => reaction.emoji.name === '🅰️')
 
-			if(archiveReaction.count > 1) return
+			if(reaction.emoji.name === '🅱️' && reaction.count > 1) return
 
-			if(reaction.emoji.name === '🅰️' && imageAttached(message)) {
+			if(reaction.emoji.name === '🅱️' && imageAttached(message)) {
 				sendArchiveMessage(
 					message,
 					message.attachments.array()[0].url,
